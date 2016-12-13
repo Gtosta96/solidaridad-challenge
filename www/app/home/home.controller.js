73669a4313a0bb040c837453634d955ed27c6947
Gtosta96 <gabrieltosta3@gmail.com>
2016-12-12 23:32:18 -0200

    finalizado para apresentação

--- www/app/home/home.controller.js
+++ www/app/home/home.controller.js
@@ -4,12 +4,38 @@
 	angular.module('test.home.controller', [])
 	.controller('HomeController', homeController);
 
-	homeController.$inject = ['$log', 'MockService'];
-	function homeController($log, MockService) {
+	homeController.$inject = ['$log', '$scope', '$ionicPopover', 'MockService'];
+	function homeController($log, $scope, $ionicPopover, MockService) {
+		
 		var vm = this;
 		vm.data = MockService.getData();
+		vm.sortProsCons = sortProsCons;
 
-		$log.log(vm.data);
+		$scope.popover = loadPopover();
+		$scope.openPopover = openPopover;
+		$scope.closePopover = closePopover;
+
+		/* Functions */
+		function sortProsCons(prosCons) {
+			return null;
+		}
+
+		/* Popover */
+		function loadPopover() {
+		  return $ionicPopover.fromTemplateUrl('app/home/popover.view.html', {
+		    scope: $scope,
+		  }).then(function (popover) {
+		    $scope.popover = popover;
+		  });
+		}
+
+		function openPopover($event) {
+			$scope.popover.show($event);
+		}
+
+		function closePopover () {
+			$scope.popover.hide();
+		}
 	}
 
 }());
--- www/app/shared/mock/mock.service.js
+++ www/app/shared/mock/mock.service.js
@@ -17,73 +17,84 @@
 	        lastName: 'Tosta',
 	        get fullName() {
 	          return this.firstName + ' ' + this.lastName;
+	        }
+	      },
+	      farm: {
+	        farmName: 'Recanto feliz',
+	        banner: {
+	          src: 'img/banner-farm.jpg'
+	        }
+	      },
+	      reports: {
+	        0: {
+	          title: 'Performance - Visão Geral',
+	          description: 'Aqui serão demonstradas as visões gerais em relação a Performance.',
+	          data: [
+							{
+								args: { type: 'accordion' },
+								title: 'Pilar Econômico',
+								content: '0 - performance',
+								chart: {
+									series: ['2015', '2016'],
+									labels: ['Informação 01', 'Informação 02', 'Informação 03'],
+									data: [
+										[65, 60, 80],
+										[49, 70, 70]
+									]
+								},
+								prosCons: {
+									pros: ['Pros 1', 'Pros 2'],
+									cons: ['Cons 1', 'Cons 2'],
+								}
+							}, {
+								args: { type: 'accordion' },
+								title: 'Pilar Ambiental',
+								content: '1 - performance',
+								chart: {
+									series: ['2015', '2016'],
+									labels: ['Informação 01', 'Informação 02', 'Informação 03'],
+									data: [
+										[65, 60, 80],
+										[49, 70, 70]
+									]
+								},
+								prosCons: {
+									pros: ['Pros 1', 'Pros 2'],
+									cons: ['Cons 1', 'Cons 2'],
+								}
+							}, {
+								args: { type: 'accordion' },
+								title: 'Pilar Social',
+								content: '2 - performance',
+								chart: {
+									series: ['2015', '2016'],
+									labels: ['Informação 01', 'Informação 02', 'Informação 03'],
+									data: [
+										[65, 60, 80],
+										[49, 70, 70]
+									]
+								},
+								prosCons: {
+									pros: ['Pros 1', 'Pros 2'],
+									cons: ['Cons 1', 'Cons 2'],
+								}
+							}
+						]
 	        },
-	        farm: {
-	          farmName: 'Recanto feliz',
-	          banner: {
-	            src: 'img/banner-farm.jpg'
-	          }
-	        },
-	        reports: {
-	          0: {
-	            title: 'Performance - Visão Geral',
-	            description: 'Aqui serão demonstradas as visões gerais em relação a Performance',
-	            data: [{
-	              title: 'Pilar Econômico',
-	              content: '0 - performance',
-	              chart: {
-	                series: ['2015', '2016'],
-	                labels: ['Informação 01', 'Informação 02', 'Informação 03'],
-	                data: [
-	                  [65, 60, 80],
-	                  [49, 70, 70]
-	                ]
-	              },
-	              prosCons: {
-	                pros: ['Pros 1', 'Pros 2'],
-	                cons: ['Cons 1', 'Cons 2'],
-	              }
-	            }, {
-	              title: 'Pilar Ambiental',
-	              content: '1 - performance',
-	              chart: {
-	                series: ['2015', '2016'],
-	                labels: ['Informação 01', 'Informação 02', 'Informação 03'],
-	                data: [
-	                  [65, 60, 80],
-	                  [49, 70, 70]
-	                ]
-	              },
-	              prosCons: {
-	                pros: ['Pros 1', 'Pros 2'],
-	                cons: ['Cons 1', 'Cons 2'],
-	              }
-	            }, {
-	              title: 'Pilar Social',
-	              content: '2 - performance',
-	              chart: {
-	                series: ['2015', '2016'],
-	                labels: ['Informação 01', 'Informação 02', 'Informação 03'],
-	                data: [
-	                  [65, 60, 80],
-	                  [49, 70, 70]
-	                ]
-	              },
-	              prosCons: {
-	                pros: ['Pros 1', 'Pros 2'],
-	                cons: ['Cons 1', 'Cons 2'],
-	              }
-	            }]
-	          },
-	          1: {
-	            title: 'Suas Respostas',
-	            description: 'Aqui serão demonstradas suas respostas em relação ao [...]'
-	          }
-	        },
+	        1: {
+	          title: 'Suas Respostas',
+	          description: 'Aqui serão demonstradas suas respostas em relação as perguntas respondidas.',
+						data: 
+						[
+							{
+								args: { type: 'table' }
+							}
+						]
+	        }
 	      }
 	    }
 	  }
-	  }
 
+	}
 
 }());
